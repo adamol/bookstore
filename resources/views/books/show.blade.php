e0c6d07c2202f5095ce2c3c34ccc4bc8b542ae94
adamol <adamol1992@gmail.com>
2017-08-31 08:40:05 +0200

    Add form to books.show view to add to cart

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -5,4 +5,11 @@
 
 <p>written by: {{ $book->author_names }}, category: {{ $book->category_names }}</p>
 <p>{{ $book->description }}</p>
+
+<form method="POST" action="/cart">
+    {{ csrf_field() }}
+    <input type="text" name="quantity">
+    <input type="hidden" name="book_id" value="{{ $book->id }}">
+    <button type="submit">Add to cart</button>
+</form>
 @endsection
